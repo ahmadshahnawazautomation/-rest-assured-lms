fb66a79add3bdd28cea5eedaf39055a4a88f8963
Shahnawaz Ahmad <ahmadshahnawaz1986@gmail.com>
2025-03-06 11:58:34 +0000

    Removing duplicate assertions

--- src/test/java/librarymanagement/apisystem/AuthorizeUser.java
+++ src/test/java/librarymanagement/apisystem/AuthorizeUser.java
@@ -38,6 +38,9 @@ public class AuthorizeUser extends AllGlobalValue {
         // Asserting the Generated Token
         Assert.assertNotNull(token, token+" :Token is not null");
         Assert.assertFalse(token.isEmpty(), token + " :Token shouldn't be empty");
+
+        validateThatUserIsAuthorized();
+        validateTokenIsGenerated();
     }
 
     /**
@@ -49,18 +52,7 @@ public class AuthorizeUser extends AllGlobalValue {
         //assert status code
         response.then().log().all().assertThat().statusCode(200);
 
-        // Holding Response to the string
-        String message = response.jsonPath().getString("message");
-
-        // Asserting message
-        Assert.assertNotNull(message, "Response " +message+ " shouldn't be null");
-        Assert.assertTrue(message.contains("Success") || message.contains("Authorized"),
-                "Authorization is successful");
-
-        // If the response contains a userId or similar key, validate it's not null
-        String userId = response.jsonPath().getString("userId");
-        Assert.assertNotNull(userId, userId +" shouldn't be null After Authorization ");
-    }
+         }
 
     /**
      * This method is used to validate the Token is generated successfully
@@ -68,16 +60,12 @@ public class AuthorizeUser extends AllGlobalValue {
     @Test (priority = 3)
     public void validateTokenIsGenerated(){
 
-        //get the token and assign it to a global variable
+        //getting the token
         token = response.path("token");
 
-        // Printing Response body to the console
-        String  responseBody  = response.getBody().asString();
-        System.out.println("This is body "+responseBody);
-
-        //Assert that response body contains token and its value is generated
+      //Assert that response body contains token and its value is generated
         Assert.assertEquals("token", "token");
         Assert.assertNotNull(token);
-    }
+        }
 
 }
