64b2330ec4a34d6e19fbd0c561d5877dc98864c3
Shahnawaz Ahmad <ahmadshahnawaz1986@gmail.com>
2025-03-06 13:02:05 +0000

    Removing duplicate assertions

--- src/test/java/librarymanagement/apisystem/AuthorizeUser.java
+++ src/test/java/librarymanagement/apisystem/AuthorizeUser.java
@@ -3,7 +3,6 @@ package librarymanagement.apisystem;
 import global.AllGlobalValue;
 import global.SessionsFilter;
 import io.restassured.http.ContentType;
-import io.restassured.response.ResponseBody;
 import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -27,7 +26,7 @@ public class AuthorizeUser extends AllGlobalValue {
         response = given().filter(new SessionsFilter()).contentType(ContentType.JSON).accept(ContentType.JSON)
                 .body(jsonObject.toString()).log().all()
                 .when()
-                    .post(getBaseUrl() + "/member/login");
+                .post(getBaseUrl() + "/member/login");
 
         // Printing Response body to the console
         String  responseBody  = response.getBody().asString();
@@ -39,47 +38,32 @@ public class AuthorizeUser extends AllGlobalValue {
         // Asserting the Generated Token
         Assert.assertNotNull(token, token+" :Token is not null");
         Assert.assertFalse(token.isEmpty(), token + " :Token shouldn't be empty");
+
+        validateThatUserIsAuthorized();
+        validateTokenIsGenerated();
     }
 
     /**
      * This method will verify the user is Authorized
      */
-    @Test (priority = 2)
     public void validateThatUserIsAuthorized() {
 
         //assert status code
         response.then().log().all().assertThat().statusCode(200);
 
-        // Holding Response to the string
-        String message = response.jsonPath().getString("message");
-
-        // Asserting message
-        Assert.assertNotNull(message, "Response " +message+ " shouldn't be null");
-        Assert.assertTrue(message.contains("Success") || message.contains("Authorized"),
-                "Authorization is successful");
-
-        // If the response contains a userId or similar key, validate it's not null
-        String userId = response.jsonPath().getString("userId");
-        Assert.assertNotNull(userId, userId +" shouldn't be null After Authorization ");
-        }
+    }
 
     /**
      * This method is used to validate the Token is generated successfully
      */
-    @Test (priority = 3)
     public void validateTokenIsGenerated(){
 
-        //get the token and assign it to a global variable
+        //getting the token
         token = response.path("token");
-
-        // Printing Response body to the console
-        String  responseBody  = response.getBody().asString();
-        System.out.println("This is body "+responseBody);
 
         //Assert that response body contains token and its value is generated
         Assert.assertEquals("token", "token");
         Assert.assertNotNull(token);
     }
 
-    }
-
+}
